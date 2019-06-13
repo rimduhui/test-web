184031140e0269840f3261997aae89c0b4218835
rimduhui <idyyy2002@navaer.com>
2019-06-13 16:55:44 +0900

    add option

--- mysite/crawl/views.py
+++ mysite/crawl/views.py
@@ -8,7 +8,6 @@ from selenium import webdriver
 def crawl() :
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
 
     driver = webdriver.Chrome("chromedriver", chrome_options=options)
